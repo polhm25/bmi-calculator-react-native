bd910ffca1b32affe6c029e856ddc1fa1d5e6b99
polhm25 <polhm25@gmail.com>
2025-11-03 20:24:56 +0100

    mi parte del proyecto terminada

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function App() {
+export default function () {
   return (
     <View>
       <Text>App</Text>
